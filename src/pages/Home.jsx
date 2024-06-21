33dace31fc08551b78d6956b5e1b0162a10166a2
casumelanie <melanie.casu-pro@outlook.fr>
2024-06-21 17:12:59 +0200

    Corrections : composant feature, correction de style, ajout sass

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,29 @@
 import bannerImg from '../assets/images/bank-tree.webp'
+import Feature from '../components/Feature.jsx'
+
+const features = [
+  {
+    image: 'src/assets/icons/icon-chat.webp',
+    imageDesc: 'Chat Icon',
+    title: 'You are our #1 priority',
+    description:
+      'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+  },
+  {
+    image: 'src/assets/icons/icon-money.webp',
+    imageDesc: 'Money Icon',
+    title: 'More savings means higher rates',
+    description:
+      'The more you save with us, the higher your interest rate will be!',
+  },
+  {
+    image: 'src/assets/icons/icon-security.webp',
+    imageDesc: 'Security Icon',
+    title: 'Security you can trust',
+    description:
+      'We use top of the line encryption to make sure your data and money is always safe.',
+  },
+]
 
 const Home = () => {
   return (
@@ -16,39 +41,17 @@ const Home = () => {
       </section>
       <section id="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src="src/assets/icons/icon-chat.webp" alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="src/assets/icons/icon-money.webp"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">
-            More savings means higher rates
-          </h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src="src/assets/icons/icon-security.webp"
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        {features.map((feature, index) => {
+          return (
+            <Feature
+              key={index}
+              image={feature.image}
+              imageDesc={feature.imageDesc}
+              title={feature.title}
+              description={feature.description}
+            />
+          )
+        })}
       </section>
     </>
   )
